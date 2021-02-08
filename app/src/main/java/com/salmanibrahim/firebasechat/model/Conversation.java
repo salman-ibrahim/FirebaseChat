ba8d86610b5ed90e5fe5c6bf8293b5af2b6c5e82
salmanibrahim <s2018065001@umt.edu.pk>
2021-02-08 20:53:47 +0500

    init

---
+++ app/src/main/java/com/salmanibrahim/firebasechat/model/Conversation.java
@@ -0,0 +1,16 @@
+package com.salmanibrahim.firebasechat.model;
+
+import java.util.ArrayList;
+
+
+
+public class Conversation {
+    private ArrayList<Message> listMessageData;
+    public Conversation(){
+        listMessageData = new ArrayList<>();
+    }
+
+    public ArrayList<Message> getListMessageData() {
+        return listMessageData;
+    }
+}
---
+++ app/src/main/java/com/salmanibrahim/firebasechat/model/Message.java
@@ -0,0 +1,11 @@
+package com.salmanibrahim.firebasechat.model;
+
+
+
+public class Message{
+    public String idSender;
+    public String idReceiver;
+    public String text;
+    public String name;
+    public long timestamp;
+}
